f54a4bb119ae0554855c0beda6055900af5967f9
godgroot <85842733+godgroot@users.noreply.github.com>
2021-10-12 16:15:29 +0530

    Create __init__.py

---
+++ LuciferMoringstar_Robot/__init__.py
@@ -0,0 +1,71 @@
+from .Utils import (
+
+   get_filter_results,
+
+   get_file_details,
+
+   is_subscribed,
+
+   get_poster,
+
+   Media
+
+)
+
+from .Channel import (
+
+   RATING,
+
+   GENRES
+
+)
+
+HELP = """
+
+<b><u>Basic Commads</u></b>
+
+➥ /start : check if am alive or dead
+
+➥ /about : About Me
+
+➥ /stats user Status
+
+<b><u>Bot Owner Only</u></b>
+
+➥ /broadcast Replay Any Message or Media
+
+➥ /ban_user  click ban_user more info
+
+➥ /unban_user click unban_user more info
+
+➥ /banned_users Banned User Details
+
+➥ /total How Many Files Added In Database
+
+➥ /logger  Get Logs
+
+➥ /delete Delete File From Database
+
+<b>[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
+
+@Mo_Tech_YT>/b>
+
+"""
+
+ABOUT = """
+
+➥ Name : CINEMAZIP BOT 😌
+
+    
+
+➥ Credits : Ashaan
+
+➥ Editing : [Muhammed RK](t.me/im_godgroot)
+
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Source Code : [Click Me](https://t.me/share/url?url=https://t.me/cinemazip)
+
+"""
